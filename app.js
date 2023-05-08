00a7085da5ba6915ada4c2de5db90ec226e55254
nikitaaldashkov <nikitastart@yandex.ru>
2023-05-09 00:19:09 +0300

    Add simple save to DB post query

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 import chalk from "chalk";
 import express from "express";
+import * as mongoose from "mongoose";
+
+import { Post } from "./Models/index.js";
 
 const app = express();
-const port = 3000;
+
+const {PORT, DB_URL} = process.env;
 
 app.use(express.json());
 
-app.post("/", function(req, res) {
-	res.json(req.body);
+app.post("/", async function(req, res) {
+	try {
+		const {author, title, content, picture} = req.body;
+		const post = await Post.create({author, title, content, picture});
+		await post.save();
+		res.json(post);
+	} catch(e) {
+		res.status(500).json(e);
+	}
 });
 
-app.listen(port, function() {
-	console.log(chalk.blue(`Server has been started on port ${port}`));
-});
+async function startApp() {
+	try {
+		await mongoose.connect(DB_URL);
+		app.listen(PORT, function() {
+			console.log(chalk.blue(`Server has been started on port ${PORT}`));
+		});
+	} catch(e) {
+		console.log(chalk.red(e));
+	}
+}
+
+await startApp();
